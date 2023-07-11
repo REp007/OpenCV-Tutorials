556ce7d947d3894c90941fe0b4e426adde0ea4f0
REp007 <elamraniy292@gmail.com>
2023-07-11 18:16:48 +0100

    .

---
+++ Chapter1.py
@@ -0,0 +1,44 @@
+'''
+    Created on 2023年07月11日
+'''
+
+# 1- Read Image, Video and Webcam
+import cv2
+
+img = cv2.imread('colorful.jpg')
+
+cv2.imshow('img', img)
+
+cv2.waitKey(0)
+
+
+
+# For Video
+
+cap = cv2.VideoCapture('src/vedio.mp4')
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow('frmVedio', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+# For Webcam
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow('frmWebcam', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
+
+
