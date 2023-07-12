9fc288e8909d08866dc17b8ac2fd4979665b10d5
REp007 <elamraniy292@gmail.com>
2023-07-12 20:07:00 +0100

    Chapter6.py

---
+++ Chapter6.py
@@ -0,0 +1,33 @@
+'''
+JOINING IMAGES
+'''
+import cv2
+import numpy as np
+
+
+img = cv2.imread('src/colorful.jpg')
+
+# Horizontal Stacking
+
+hor = np.hstack((img, img))
+
+cv2.imshow('Horizontal', hor)
+cv2.imwrite('Resources/Chapter6/hor.jpg', hor)
+
+
+# Vertical Stacking
+
+ver = np.vstack((img,img))
+
+cv2.imshow('Vertical', ver)
+cv2.imwrite('Resources/Chapter6/ver.jpg', ver)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# TODO : Function StackingImages()
+
+def StackingImages():
+    pass
